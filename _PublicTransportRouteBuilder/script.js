966b413c4f9d85270796fc44eb880cd7fc914339
Iliort <igolod@live.com>
2016-01-16 10:37:44 +0300

    2
    CScript deleted, id properties

--- _PublicTransportRouteBuilder/script.js
+++ _PublicTransportRouteBuilder/script.js
@@ -1,31 +1,7 @@
-function RouteMap (coords, links)
-{
-    this.coords = coords;
+function RouteMap (placemarks, links) {
+    this.placemarks = placemarks;
     this.links = links;
 }
-
-function StationMark(coords, id)
-{
-    this.id = id;
-    this.coords = coords;
-    //this.prototype = new ymaps.Placemark(coords);
-    this.events.add('contextmenu', function(e)
-    {
-        if (!isSelected) {
-            tempId = this.id;
-            isSelected = true;
-        }
-        else
-        {
-            isSelected = false;
-            routeMap.links.push([tempId, this.id]);
-        }
-        console.log(routeMap);
-    });
-
-
-}
-//StationMark.prototype = new ymaps.Placemark();
 
 ymaps.ready(init);
 
@@ -36,7 +12,6 @@ var curId = 0;
 function init () {
 
     var map;
-
 
     map = new ymaps.Map("map", {
         center: [57.5262, 38.3061], // Углич
@@ -50,10 +25,24 @@ function clickHandling(map, routeMap)
     map.events.add('click', function(e)
     {
         var pos = e.get('coords');
-        map.geoObjects.add(new ymaps.Placemark(pos, curId));
-        routeMap.coords.push(pos);
+        var placemark = new ymaps.Placemark(pos, undefined, {
+            preset: 'islands#circleDotIcon',
+            iconColor: '#1faee9'
+        });
+
+        placemark.properties.set('ID', curId);
+        addSelectionEvent(placemark);
+        console.log(placemark.properties.get('ID'));
+
+        map.geoObjects.add(placemark);
+        routeMap.placemarks.push(placemark);
         curId++;
-        console.log(routeMap.coords);
     });
 }
 
+function addSelectionEvent(placemark)
+{
+    placemark.events.add('contextmenu', function(e){
+        e.get('target').options.set('preset', 'islands#circleIcon');
+    });
+}
